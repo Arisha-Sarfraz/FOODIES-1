9d24f1aad0efbacdc978e95aa48d1b3f15863af7
Qura-tul-ain <38294643+Qura-tul-ain@users.noreply.github.com>
2018-04-15 10:05:13 -0700

    This class has three attributes.
    Class contains getters setters  of attributes as well. Items are added by the admin.

---
+++ ClassItems.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ooad
+{
+    class ClassItems
+    {
+        private string itemname;
+        private string category;
+        private double price;
+
+        public string Itemname
+        {
+            get
+            {
+                return itemname;
+            }
+
+            set
+            {
+                itemname = value;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+
+            set
+            {
+                category = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
