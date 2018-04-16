d25be5fe2196f6573c9040bb3fd21f46e075205a
Arisha-Sarfraz <38426892+Arisha-Sarfraz@users.noreply.github.com>
2018-04-16 22:43:17 +0500

    class status

---
+++ Status.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAreesha
+{
+    class Status
+    {
+        private string username;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        private bool ispaid;
+
+        public bool Ispaid
+        {
+            get { return ispaid; }
+            set { ispaid = value; }
+        }
+        private bool isdelivered;
+
+        public bool Isdelivered
+        {
+            get { return isdelivered; }
+            set { isdelivered = value; }
+        }
+
+    }
+}
